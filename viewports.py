21e818ae7c0ce7f2958c9e00c72789c587410fc1
Adaephon-GH <adaephon@gmx.net>
2017-01-26 18:07:33 +0100

    Start using Pillow instead of wand

--- viewports.py
+++ viewports.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 import re
-from wand.image import Image
-from wand.display import display
-from wand.color import Color
+from PIL import Image
 
 
 class Rectangle:
@@ -90,101 +88,99 @@ class Viewport:
         self.scale = self.dpi / viewport.dpi
 
 
-class VPImage(Image):
-    def __init__(self, rectangle=None, **kwargs):
-        if rectangle is not None:
-            if any(kwargs.get(key, None) is not None
-                   for key in ["width", "height"]):
-                raise TypeError(
-                    "rectangle is mutually exclusive to width or height")
-            super().__init__(width=rectangle.width, height=rectangle.height,
-                             **kwargs)
-        else:
-            super().__init__(**kwargs)
-
-    def rcomposite(self, image, rectangle):
-        self.composite(image, rectangle.x, rectangle.y)
+# class VPImage(Image):
+#     def __init__(self, rectangle=None, **kwargs):
+#         if rectangle is not None:
+#             if any(kwargs.get(key, None) is not None
+#                    for key in ["width", "height"]):
+#                 raise TypeError(
+#                     "rectangle is mutually exclusive to width or height")
+#             super().__init__(width=rectangle.width, height=rectangle.height,
+#                              **kwargs)
+#         else:
+#             super().__init__(**kwargs)
+#
+#     def rcomposite(self, image, rectangle):
+#         self.composite(image, rectangle.x, rectangle.y)
 
 
 def dummy_tester():
-    with Image(filename='sample.png') as orig:
-        with Image(width=640, height=360, background=Color('gray')) as img:
-            with orig.clone() as a:
-                a.transform('50x50+100+100', '100')
-                img.composite(a, 30,70)
-            with orig.clone() as b:
-                b.transform('100x100+100+100', '50%')
-                img.composite(b, 300,70)
-            with orig.clone() as c:
-                c.transform('200x50+10+30', '75%')
-                img.composite(c, 200,200)
-            display(img)
+    with Image.open('sample.png') as orig:
+        with Image.new('RGB', (640, 360), color='gray') as img:
+            with orig.crop((100, 100, 150, 150)) as a:
+                img.paste(a, (30, 70))
+            with orig.crop((100, 100, 200, 200)) as b:
+                b.resize((50, 50))
+                img.paste(b, (300, 70))
+            with orig.crop((10, 30, 210, 230)) as c:
+                img.paste(c, (round(200 * .75), round(200 * .75)))
+            img.show()
 
 
-def overlap_tester():
-    import itertools
-    bigrect = ScreenRectangle(1000, 1000)
-    redrect = ScreenRectangle(300, 300, 350, 350)
-    for w, h in itertools.combinations_with_replacement([50, 250, 500], 2):
-        for x, y in itertools.combinations_with_replacement([50, 400, 700], 2):
-            greenrect = ScreenRectangle(w, h, x, y)
-            outerrect = redrect | greenrect
-            innerrect = redrect & greenrect
-            with VPImage(rectangle=bigrect, background=Color("gray")) as img:
-                if outerrect:
-                    with VPImage(rectangle=outerrect,
-                                 background=Color("#7f7f00")) as n:
-                        img.rcomposite(n, outerrect)
-                if innerrect:
-                    with VPImage(rectangle=innerrect,
-                                 background=Color("#0000ff")) as n:
-                        img.rcomposite(n, innerrect)
-                display(img)
-                with VPImage(rectangle=redrect,
-                             background=Color("#ff00007f")) as m:
-                    img.rcomposite(m, redrect)
-                with VPImage(rectangle=greenrect,
-                             background=Color("#00ff007f")) as n:
-                    img.rcomposite(n, greenrect)
-                display(img)
-
-
-sampleLayout = {
-    "Left":
-        Viewport(PhysicalRectangle(518, 324, 0, 86.4),
-                 ScreenRectangle.from_geometry_string('1920x1200+0+320')),
-    "Middle":
-        Viewport(PhysicalRectangle(518, 324, 538, 0),
-                 ScreenRectangle.from_geometry_string('1920x1200+1920+0')),
-    "Right":
-        Viewport(PhysicalRectangle(475, 267, 1096, 28.5),
-                 ScreenRectangle.from_geometry_string('1920x1080+3840+60')),
-    "Laptop":
-        Viewport(PhysicalRectangle(346, 194, 624, 384),
-                 ScreenRectangle.from_geometry_string('1920x1080+1920+1200')),
-}
-
-
-def show_layout(layout):
-    from functools import reduce
-    import operator
-
-    scrv = reduce(operator.or_, [layout[k].screen for k in layout])
-    phyv = reduce(operator.or_, [layout[k].physical for k in layout])
-
-    with VPImage(rectangle=scrv, background=Color("gray")) as img:
-        for p in [round(layout[k].screen) for k in layout]:
-            with VPImage(rectangle=p, background=Color("red")) as s:
-                img.rcomposite(s, p)
-        img.transform(resize="500")
-        display(img)
-
-    with VPImage(rectangle=phyv, background=Color("gray")) as img:
-        for p in [round(layout[k].physical) for k in layout]:
-            with VPImage(rectangle=p, background=Color("red")) as s:
-                img.rcomposite(s, p)
-        img.transform(resize="500")
-        display(img)
+# def overlap_tester():
+#     import itertools
+#     bigrect = ScreenRectangle(1000, 1000)
+#     redrect = ScreenRectangle(300, 300, 350, 350)
+#     for w, h in itertools.combinations_with_replacement([50, 250, 500], 2):
+#         for x, y in itertools.combinations_with_replacement([50, 400, 700], 2):
+#             greenrect = ScreenRectangle(w, h, x, y)
+#             outerrect = redrect | greenrect
+#             innerrect = redrect & greenrect
+#             with VPImage(rectangle=bigrect, background=Color("gray")) as img:
+#                 if outerrect:
+#                     with VPImage(rectangle=outerrect,
+#                                  background=Color("#7f7f00")) as n:
+#                         img.rcomposite(n, outerrect)
+#                 if innerrect:
+#                     with VPImage(rectangle=innerrect,
+#                                  background=Color("#0000ff")) as n:
+#                         img.rcomposite(n, innerrect)
+#                 display(img)
+#                 with VPImage(rectangle=redrect,
+#                              background=Color("#ff00007f")) as m:
+#                     img.rcomposite(m, redrect)
+#                 with VPImage(rectangle=greenrect,
+#                              background=Color("#00ff007f")) as n:
+#                     img.rcomposite(n, greenrect)
+#                 display(img)
+#
+#
+# sampleLayout = {
+#     "Left":
+#         Viewport(PhysicalRectangle(518, 324, 0, 86.4),
+#                  ScreenRectangle.from_geometry_string('1920x1200+0+320')),
+#     "Middle":
+#         Viewport(PhysicalRectangle(518, 324, 538, 0),
+#                  ScreenRectangle.from_geometry_string('1920x1200+1920+0')),
+#     "Right":
+#         Viewport(PhysicalRectangle(475, 267, 1096, 28.5),
+#                  ScreenRectangle.from_geometry_string('1920x1080+3840+60')),
+#     "Laptop":
+#         Viewport(PhysicalRectangle(346, 194, 624, 384),
+#                  ScreenRectangle.from_geometry_string('1920x1080+1920+1200')),
+# }
+#
+#
+# def show_layout(layout):
+#     from functools import reduce
+#     import operator
+#
+#     scrv = reduce(operator.or_, [layout[k].screen for k in layout])
+#     phyv = reduce(operator.or_, [layout[k].physical for k in layout])
+#
+#     with VPImage(rectangle=scrv, background=Color("gray")) as img:
+#         for p in [round(layout[k].screen) for k in layout]:
+#             with VPImage(rectangle=p, background=Color("red")) as s:
+#                 img.rcomposite(s, p)
+#         img.transform(resize="500")
+#         display(img)
+#
+#     with VPImage(rectangle=phyv, background=Color("gray")) as img:
+#         for p in [round(layout[k].physical) for k in layout]:
+#             with VPImage(rectangle=p, background=Color("red")) as s:
+#                 img.rcomposite(s, p)
+#         img.transform(resize="500")
+#         display(img)
 
 
 if __name__ == "__main__":
